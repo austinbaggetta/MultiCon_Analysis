e13e2a3f3135f5b2453cae92b4a9a90a3b6d17f7
austinbaggetta <austin.baggetta@icahn.mssm.edu>
2022-09-06 16:43:34 -0400

    Added new functions

--- circletrack_behavior.py
+++ circletrack_behavior.py
@@ -3,36 +3,41 @@ import numpy as np
 import os
 import re
 import glob
+from os.path import join as pjoin
 
-## Get list of circle_track.csv files by setting rpath
-## Example rpath: r'/media/caishuman/csstorage3/Austin/CircleTrack/LargeScale/MultiCon_Verification/Data/**/**/**/*.csv'
+
 def get_file_list(rpath):
+    ## Get list of circle_track.csv files by setting rpath
+    ## Example rpath: r'/media/caishuman/csstorage3/Austin/CircleTrack/LargeScale/MultiCon_Verification/Data/**/**/**/circle_track.csv'
     ## Create list of files
     file_list = glob.glob(rpath)
     return file_list
 
-## Used to extract mouse name from file string
-## Use a for loop to extract mouse name from every file in file_list
-## Example str2match: r'(mcv[0-9]+)'
+
 def get_mouse(file_list, str2match):
+    ## Used to extract mouse name from file string
+    ## Use a for loop to extract mouse name from every file in file_list
+    ## Example str2match: r'(mcv[0-9]+)'
     mouseID = re.search(str2match, file_list)
     return mouseID.group()
 
-## Combine the whole file_list with mouse_list
-## Combines both into a data frame with filepath and mouse column names
+
 def combine(file_list, mouse_list):
+    ## Combine the whole file_list with mouse_list
+    ## Combines both into a data frame with filepath and mouse column names
     files = pd.DataFrame(file_list, columns = ['filepath'])
     mice = pd.DataFrame(mouse_list, columns = ['mouse'])
     combined = pd.concat([files, mice], axis = 1)
     return combined
 
-## Subsets combined file_list and mouse_list based on mouse string
+
 def subset_combined(data, mouse):
+    ## Subsets combined file_list and mouse_list based on mouse string
     files = data.loc[data.mouse == mouse]
     file_list = files.filepath
     return file_list
 
-## Crop data from START to TERMINATE
+
 def crop_data(data):
     ## Get data from START to TERMINATE
     if any(data.event == 'TERMINATE'):
@@ -41,8 +46,9 @@ def crop_data(data):
         df = data[np.where(data['event'] == 'START')[0][0]:]
     return df
 
-## Get rewarding ports
+
 def get_rewarding_ports(data):
+    ## Get rewarding ports
     ## In more recent circle track experiments, initialization was added to easily get rewarding ports
     ## Previous experiments require assessing from rewarding licks
     if any(data.event == 'initializing'):
@@ -57,19 +63,87 @@ def get_rewarding_ports(data):
         reward_second = reward_ports.iloc[1, 0]
         return reward_first, reward_second
 
-## Get licks
+
 def get_licks(data):
+    ## Get licks
     lick_tmp = data.loc[(data.event == 'LICK') | (data.event == 'REWARD')].replace(to_replace='REWARD', value='LICK').reset_index(drop = True)
     return lick_tmp
 
-## Normalize timestamp to get seconds
+
+def align_behavior_frames(data, time, plot_frame_usage=False):
+    """
+    Takes timestamps matrix associated with a miniscope recording and a regularly spaced time vector the
+    expected length of the session. For each timeframe in 'time', the closest frame from minian_timestamps is acquired.
+    Some frames are used more than once. Returns vector of lined up frames to use to align recording to the time vector.
+    """
+    lined_up_timeframes = np.array(
+        [
+            np.abs(minian_timestamps["Time Stamp (ms)"] - (t * 1000)).argmin()
+            for t in time
+        ]
+    )
+
+    if plot_frame_usage:
+        duplicated_timeframes = np.unique(lined_up_timeframes, return_counts=True)
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scattergl(
+                x=duplicated_timeframes[0],
+                y=duplicated_timeframes[1],
+                mode="lines+markers",
+                marker_size=5,
+            )
+        )
+        fig.update_layout(
+            template="simple_white",
+            xaxis_title="Time (ms)",
+            yaxis_title="Frequency",
+            title_text="Number of times each miniscope frame was re-used",
+        )
+        fig.show(config={"scrollZoom": True})
+
+    return lined_up_timeframes
+
+
+def load_and_align_behavior(dpath, mouse, date, timestamp, session = '20min', sampling_rate=1 / 15):
+    """
+    Parameters:
+    ==========
+    dpath : str
+        directory path
+    mouse : str
+        name of the mouse (e.g. 'mc01')
+    date : str
+        date of session
+    timestamp : str
+        timestamp associated with session
+    session : str
+        one of ['20min'], may incorporate other options if length of sessions decrease from 20min
+    """
+    # create time vector based on expected length of session
+    if "20min" in session:
+        frame_count = (
+            20 * 60 / sampling_rate
+        )  # 20min x 60sec/min / sampling_rate (usually 1/15 because of temporal downsampling during Minian processing)
+    else:
+        raise Exception(
+            "Invalid 'session' argument. Must be one of: ['20min']"
+        )
+    time = np.arange(0, frame_count * sampling_rate, sampling_rate)
+    ## Load behavior data
+    rpath = pjoin(dpath, '/Data/{}/{}/{}/circle_track.csv'.format(mouse, date, timestamp))
+    mouse_behavior = pd.read_csv(rpath)
+    
+    
 def normalize_timestamp(data):
+    ## Normalize timestamp to get seconds
     time = float(data.timestamp.loc[data.event == 'START'])
     data.timestamp = (data.timestamp - time)
     return data
 
-## Calculate lick accuracy
+
 def calculate_lick_accuracy(lick_tmp, reward_first, reward_second):
+    ## Calculate lick accuracy
     ## lick_tmp is a data frame from the output of the get_licks function
     ## reward_first and reward_second can either be manually input, or can be determined from get_rewarding_ports function
     ## Shift data column, corresponds to licks at reward port
@@ -88,9 +162,10 @@ def calculate_lick_accuracy(lick_tmp, reward_first, reward_second):
     percent_correct = ((licks.event[(licks.data == reward_first) | (licks.data == reward_second)].dropna().sum()) / licks.event.dropna().sum()) * 100
     return percent_correct
 
-## Calculate total rewards for a mouse across all of that mouse's sessions
-## Can be combined with a for loop to get entire cohort's total rewards
+
 def get_total_rewards(file_list, mouse):
+    ## Calculate total rewards for a mouse across all of that mouse's sessions
+    ## Can be combined with a for loop to get entire cohort's total rewards
     ## Subset file_list based on mouse
     files = subset_combined(file_list, mouse)
     ## Initialize empty data frame
@@ -113,10 +188,11 @@ def get_total_rewards(file_list, mouse):
     reward_data = pd.DataFrame(reward_data)
     return reward_data
 
-## Calculate first lick accuracy
-## Selects the first lick in a bout of licks, calculates the number of first licks per rewarding port
-## Subsequently divides the sum of the first licks at the two rewarding ports by the total number of first licks
+
 def get_lick_accuracy(file_list, mouse):
+    ## Calculate first lick accuracy
+    ## Selects the first lick in a bout of licks, calculates the number of first licks per rewarding port
+    ## Subsequently divides the sum of the first licks at the two rewarding ports by the total number of first licks
     ## Subset file_list based on mouse
     files = subset_combined(file_list, mouse)
     ## Initialize empty data frame
@@ -143,8 +219,9 @@ def get_lick_accuracy(file_list, mouse):
     lick_data = pd.DataFrame(lick_data)
     return lick_data
 
-## Get x, y, and angular position of the mouse across the sessions
+
 def get_location_data(file_list, mouse):
+    ## Get x, y, and angular position of the mouse across the sessions
     ## Subset file_list based on mouse
     files = subset_combined(file_list, mouse)
     ## Initialize empty data frame
--- circletrack_neural.py
+++ circletrack_neural.py
@@ -1,15 +1,97 @@
+import os
+import pickle
+from os import listdir
+from os.path import isdir
+from os.path import join as pjoin
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import pylab as pl
+import xarray as xr
+from numpy.polynomial.polynomial import polyfit
+from plotly.subplots import make_subplots
+from scipy.ndimage.filters import gaussian_filter, gaussian_filter1d
+from scipy.stats import pearsonr, zscore
 
 
-def load_and_align_minian(
-    mouse, session, neural_type="spikes", sigma=None, sampling_rate=1 / 30
-):
+def open_minian(dpath, post_process=None, return_dict=False):
+    """
+    Opens a file previously saved in minian handling the proper data format and chunks
+    Args:
+        dpath ([string]): contains the normalized absolutized version of the pathname path,which is the path to minian folder;
+        Post_process (function): post processing function, parameters: dataset (xarray.DataArray), mpath (string, path to the raw backend files)
+        return_dict ([boolean]): default False
+    Returns:
+        xarray.DataArray: [loaded data]
+    """
+    dslist = [
+        xr.open_zarr(pjoin(dpath, d))
+        for d in os.listdir(dpath)
+        if isdir(pjoin(dpath, d)) and d.endswith(".zarr")
+    ]
+    if return_dict:
+        dslist = [list(d.values())[0] for d in dslist]
+        ds = {d.name: d for d in dslist}
+    else:
+        ds = xr.merge(dslist, compat="no_conflicts")
+    if (not return_dict) and post_process:
+        ds = post_process(ds, dpath)
+    return ds
+
+
+def align_miniscope_frames(minian_timestamps, time, plot_frame_usage=False):
+    """
+    Takes timestamps matrix associated with a miniscope recording and a regularly spaced time vector the
+    expected length of the session. For each timeframe in 'time', the closest frame from minian_timestamps is acquired.
+    Some frames are used more than once. Returns vector of lined up frames to use to align recording to the time vector.
+    """
+    lined_up_timeframes = np.array(
+        [
+            np.abs(minian_timestamps["Time Stamp (ms)"] - (t * 1000)).argmin()
+            for t in time
+        ]
+    )
+
+    if plot_frame_usage:
+        duplicated_timeframes = np.unique(lined_up_timeframes, return_counts=True)
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scattergl(
+                x=duplicated_timeframes[0],
+                y=duplicated_timeframes[1],
+                mode="lines+markers",
+                marker_size=5,
+            )
+        )
+        fig.update_layout(
+            template="simple_white",
+            xaxis_title="Time (ms)",
+            yaxis_title="Frequency",
+            title_text="Number of times each miniscope frame was re-used",
+        )
+        fig.show(config={"scrollZoom": True})
+
+    return lined_up_timeframes
+
+
+def load_and_align_minian(path, mouse, date, timestamp, session = '20min', neural_type="spikes", sigma=None, sampling_rate=1 / 30):
     """
     Parameters:
     ==========
+    dpath : str
+        experiment directory
     mouse : str
-        name of the mouse (e.g. 'RLI5-1')
+        name of the mouse (e.g. 'mc01')
+    date : str
+        date of session
+    timestamp : str
+        timestamp of session
     session : str
-        one of ['NeutralExposure', 'FC', 'Recall1', 'Recall2', 'Recall3', 'OfflineDay1', 'OfflineDay2']
+        one of ['20min'], may incorporate other options if length of sessions decrease from 20min
     neural_type : str
         one of ['traces', 'spikes', 'smoothed']
     sigma : int
@@ -17,25 +99,19 @@ def load_and_align_minian(
     """
 
     # create time vector based on expected length of session
-    if "NeutralExposure" in session:
+    if "20min" in session:
         frame_count = (
-            10 * 60 / sampling_rate
-        )  # 10min x 60sec/min / sampling_rate (usually 1/30)
-    elif "Offline" in session:
-        frame_count = 60 * 60 / sampling_rate
-    elif "Recall" in session:
-        frame_count = 5 * 60 / sampling_rate
-    elif "FC" in session:
-        frame_count = 274 / sampling_rate
+            20 * 60 / sampling_rate
+        )  # 20min x 60sec/min / sampling_rate (usually 1/15 because of temporal downsampling during Minian processing)
     else:
         raise Exception(
-            "Invalid 'session' argument. Must be one of: ['NeutralExposure', 'FC', 'Recall1', 'Recall2', 'Recall3', 'OfflineDay1', 'OfflineDay2']"
+            "Invalid 'session' argument. Must be one of: ['20min']"
         )
     time = np.arange(0, frame_count * sampling_rate, sampling_rate)
 
     # load the specified type of neural activity
-    dpath = get_dpath(mouse, session)
-    mouse_minian = open_minian(dpath + "/minian", return_dict=True)
+    rpath = pjoin(path, 'Results/{}/{}/{}/minian'.format(mouse, date, timestamp))
+    mouse_minian = open_minian(rpath, return_dict=True)
     if neural_type == "traces":
         neural_activity = mouse_minian["C"]
     elif (neural_type == "spikes") or (neural_type == "smoothed"):
@@ -44,8 +120,8 @@ def load_and_align_minian(
         raise Exception(
             "Not a valid 'neural_type'; must be one of ['traces', 'spikes', 'smoothed']."
         )
-
-    minian_timestamps = pd.read_csv(dpath + "/timeStamps.csv")
+    tpath = pjoin(path, 'Data/{}/{}/{}/miniscope'.format(mouse, date, timestamp))
+    minian_timestamps = pd.read_csv(tpath + "/timeStamps.csv")
     lined_up_timeframes = align_miniscope_frames(minian_timestamps, time)
     neural_activity = neural_activity.sel(frame=lined_up_timeframes)
     if (
@@ -54,3 +130,4 @@ def load_and_align_minian(
         neural_activity = gaussian_filter(neural_activity, sigma=(1, sigma))
 
     return neural_activity
+
