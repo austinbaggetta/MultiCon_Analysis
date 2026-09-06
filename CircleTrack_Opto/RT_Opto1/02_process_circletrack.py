670b269ee37664e471ebe4e6625e043849e6ebd6
austinbaggetta <austin.baggetta@icahn.mssm.edu>
2026-09-06 11:55:19 -0400

    Added exclusion of rto12 due to context error

--- CircleTrack_Opto/RT_Opto1/02_process_circletrack.py
+++ CircleTrack_Opto/RT_Opto1/02_process_circletrack.py
@@ -15,7 +15,7 @@ import plotting_functions as pf
 
 # %%
 ## Set parameters
-starting_idx = 3 ## can use to specify which days you want processed
+starting_idx = 4 ## can use to specify which days you want processed
 parent_dir = 'CircleTrack_Opto'
 experiment_dir = 'RT_Opto1'
 todays_mazes = pd.read_csv(f'../../../{parent_dir}/{experiment_dir}/maze_yml/{experiment_dir} - TodaysMazes.csv')
@@ -26,7 +26,7 @@ save_path = os.path.abspath(f"../../../{parent_dir}/{experiment_dir}/output/beha
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 cohort_name = 'rto1'
-mouse_list = [f'rto0{x}' for x in np.arange(2, 10)] + ['rto10', 'rto11', 'rto12']
+mouse_list = [f'rto0{x}' for x in np.arange(2, 10)] + ['rto10', 'rto11']
 str2match = "(rto[0-9]+)" ## Set str2match variable (regex for mouse name)
 ## Set relative path variable for circletrack behavior data
 csv_path = pjoin(behavior_path, "data/**/**/**/circle_track.csv")
